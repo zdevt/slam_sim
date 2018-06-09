1982d666d6cf2542da491682e68aa17274ebc5ff
zdevt <zhangtao.hawk@qq.com>
2018-06-09 13:41:45 +0800

    move robot

--- slam.py
+++ slam.py
@@ -6,7 +6,7 @@
 #
 #        Version:  1.0
 #        Created:  2018-06-06 16:53:32
-#  Last Modified:  2018-06-08 14:06:28
+#  Last Modified:  2018-06-09 13:41:37
 #       Revision:  none
 #       Compiler:  gcc
 #
@@ -23,15 +23,14 @@ from PIL import Image
 display_width = 800
 display_height = 600
 display_res = (display_width, display_height)
-
+robotsize = (100, 100)
 FPS = 10
 
+slam = {}
 pic = {}
-global screen
-global fpsclock
 
 
-def formatpic(file):
+def convertPicToAlpha(file):
     im = Image.open(file)
     x, y = im.size
     p = Image.new('RGBA', im.size, (0, 0, 0))
@@ -44,6 +43,13 @@ def loadres():
         pygame.image.load("shape_sample/%d.png" % (i + 1)).convert()
         for i in range(0, 20)
     ]
+
+
+def loadRobot():
+    img = pygame.transform.smoothscale(
+        pygame.image.load('000.png').convert(), robotsize)
+    img.set_colorkey((0, 0, 0))
+    return img
 
 
 def getRandomPic():
@@ -71,108 +77,83 @@ class MySprite(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = cordinate
         self.mask = pygame.mask.from_surface(self.image)
 
+    def setxy(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
 
-def test():
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                exit()
+    def draw(self, s):
+        s.blit(self.image, (self.rect.x, self.rect.y))
 
-        screen.fill((0, 255, 0))
+    def up(self):
+        self.rect.y -= 5
 
-        gbo = genBlockObject()
-        pic1, p1 = gbo[0]
-        sp1 = MySprite(pic1, p1)
-        screen.blit(pic1, p1)
-        pic2, p2 = gbo[1]
-        sp2 = MySprite(pic2, p2)
-        screen.blit(pic2, p2)
-        pygame.display.update()
-        fpsclock.tick(FPS)
+    def down(self):
+        self.rect.y += 5
 
-        if pygame.sprite.collide_mask(sp1, sp2):
-            print("collide:", p1, p2)
-            raw_input()
+    def left(self):
+        self.rect.x -= 5
+
+    def right(self):
+        self.rect.x += 5
 
 
-def test2():
-    while True:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                exit()
-
-        fpsclock.tick(FPS)
-        screen.fill((0, 0, 0))
-
-        p1, c1 = getRandomPic()
-        p2, c2 = getRandomPic()
-
-        s1 = MySprite(p1, c1)
-        s2 = MySprite(p2, c2)
-
-        pygame.draw.rect(screen, (255, 0, 0), s1.rect, 1)
-        pygame.draw.rect(screen, (255, 0, 0), s2.rect, 1)
-
-        screen.blit(p1, c1)
-        screen.blit(p2, c2)
-
-        if pygame.sprite.collide_mask(s1, s2):
-            print("  cordinate:", c1, c2)
-            print("sprite rect:", s1.rect, s2.rect)
-            print(s1.mask.count(), s2.mask.count())
-            # raw_input()
-
-        raw_input()
-        pygame.display.update()
+def getRobotSprite():
+    slam['robot'] = MySprite(loadRobot(), (0, 0))
 
 
-def genBlockObjectsprite():
-    screen.fill((0, 0, 0))
-
+def getBlockObjectSpriteGroup():
     p1, c1 = getRandomPic()
     s1 = MySprite(p1, c1)
 
-    sGroup = pygame.sprite.Group()
-    sGroup.add(s1)
-
-    screen.blit(p1, c1)
-    pygame.draw.rect(screen, (255, 0, 0), s1.rect, 1)
+    g1 = pygame.sprite.Group()
+    g1.add(s1)
 
     for i in xrange(10):
         p, c = getRandomPic()
         s = MySprite(p, c)
-        if pygame.sprite.spritecollide(s, sGroup, False,
+        if pygame.sprite.spritecollide(s, g1, False,
                                        pygame.sprite.collide_mask):
-            print("collide detected")
+            # print("collide detected ",s.rect)
+            pass
         else:
-            sGroup.add(s)
-            screen.blit(p, c)
-            pygame.draw.rect(screen, (255, 0, 0), s.rect, 1)
+            g1.add(s)
 
-    pygame.display.update()
-    return sGroup
+    slam['g1'] = g1
+
+
+def moveIt(s, key):
+    if key[pygame.K_w]:
+        s.up()
+    elif key[pygame.K_s]:
+        s.down()
+    elif key[pygame.K_a]:
+        s.left()
+    elif key[pygame.K_d]:
+        s.right()
 
 
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode(display_res, 0, 32)
-    screen.fill((0, 0, 0))
+    slam['screen'] = pygame.display.set_mode(display_res, 0, 32)
     pygame.display.set_caption('slam')
-    pygame.display.update()
-    fpsclock = pygame.time.Clock()
-
-    # for i in xrange(0, 20):
-    # formatpic('shape_sample/%d.png' % (i + 1))
+    slam['fpsclock'] = pygame.time.Clock()
 
     loadres()
-    # test()
-    # test2()
-    genBlockObjectsprite()
+    getBlockObjectSpriteGroup()
+    getRobotSprite()
+
+    if pygame.sprite.spritecollide(slam['robot'], slam['g1'], False,
+                                   pygame.sprite.collide_mask):
+        print("collide", slam['robot'])
 
     while True:
+        slam['screen'].fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 exit()
+        key = pygame.key.get_pressed()
+        moveIt(slam['robot'], key)
+        slam['g1'].draw(slam['screen'])
+        slam['robot'].draw(slam['screen'])
+        pygame.display.update()
