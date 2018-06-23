551bd54f98e129aa833f49c586e7b0245ba59f25
zdevt <zhangtao.hawk@qq.com>
2018-06-23 10:36:32 +0800

    demo 0.1

--- my_astar.py
+++ my_astar.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-#       FileName:  my_astar.py
+#       FileName:  my_astar2.py
 #
 #    Description:
 #
 #        Version:  1.0
-#        Created:  2018-06-19 16:22:23
-#  Last Modified:  2018-06-21 11:45:25
+#        Created:  2018-06-21 13:37:41
+#  Last Modified:  2018-06-22 09:57:29
 #       Revision:  none
 #       Compiler:  gcc
 #
@@ -14,113 +14,188 @@
 #   Organization:
 
 import os
+import sys
+import copy
 import math
 import pygame
-from pygame.locals import *
-from sys import exit
 import random
+
+from sys import exit
 from PIL import Image
+from pygame.locals import *
 
 delta = 20
 
 display_width, display_height = 800, 600
 display_res = (display_width + 1, display_height + 1)
 
-openlist = []
-closedlist = []
-tracklist = []
-maplist = {}
-
-BLOCKCNT = int((display_width / delta) * (display_height / delta) / 8)
-
-robotsize = (delta, delta)
+BLOCKCNT = int((display_width / delta) * (display_height / delta) / 10)
 
 xcnt = int((display_width / delta) - 1)
 ycnt = int((display_height / delta) - 1)
 
-global SCREEN, FPSCLOCK
-FPS = 20
+SCREEN = None
+FPSCLOCK = None
+
+FPS = 10
 
 WHITE = (255, 255, 255)
 
+start = None
+end = None
 
-class Cell:
-    def __init__(self, pos):
-        self.pos = pos
-        self.G = 0
-        self.H = 0
-        self.F = 0
-        self.flag = 0  # 0:空 1:障碍 2:起点 3: 终点 4:轨迹
+block_list = []
+tracklist = []
 
-    def getPos(self):
-        return self.pos
+tracklistRec = {}
 
-    def setEmpty(self):
-        self.flag = 0
+open_list = {}
+close_list = {}
 
-    def isEmpty(self):
-        return (0 == self.flag)
 
-    def setBlock(self):
-        self.flag = 1
+class Node:
+    def __init__(self, father, x, y):
+        self.father = father
 
-    def isBlock(self):
-        return (1 == self.flag)
+        self.x = x
+        self.y = y
 
-    def setStart(self):
-        self.flag = 2
+        if father != None:
+            G2father = calc_G(father, self)
+            self.G = G2father + father.G
+            self.H = calc_H(self, end)
+            self.F = self.G + self.H
+        else:
+            self.G = 0
+            self.H = 0
+            self.F = 0
 
-    def isStart(self):
-        return (2 == self.flag)
+    def reset_father(self, father, new_G):
+        if father != None:
+            self.G = new_G
+            self.F = self.G + self.H
 
-    def setEnd(self):
-        self.flag = 3
+        self.father = father
 
-    def isEnd(self):
-        return (3 == self.flag)
 
-    def setTrack(self):
-        self.flag = 4
+def checkValid(x, y):
+    if 0 <= x <= xcnt and 0 <= y <= ycnt:
+        return True
+    return False
 
-    def isTrack(self):
-        return (4 == self.flag)
 
-    def getPosCell(self, pos):
-        (x, y) = pos
-        if 0 <= x <= xcnt and 0 <= y <= ycnt:
-            return maplist[(x, y)]
+def calc_G(node1, node2):
+    x1 = abs(node1.x - node2.x)
+    y1 = abs(node1.y - node2.y)
+    if (x1 == 1 and y1 == 0):
+        return 10
+    elif (x1 == 0 and y1 == 1):
+        return 10
+    # elif (x1 == 1 and y1 == 1):
+    # return 14
+    else:
+        return 0
+
+
+def calc_H(cur, end):
+    return abs(end.x - cur.x) + abs(end.y - cur.y)
+
+
+def min_F_node():
+    k = sorted(open_list.items(), key=lambda x: x[1], reverse=False)
+
+    if len(k) > 0:
+        return open_list[k[0][0]]
+
+    return False
+
+
+def addAdjacentIntoOpen(node):
+    if not node:
         return False
 
-    def getUp(self):
-        (x, y) = self.pos
-        return getPosCell((x, y - 1))
+    open_list.pop((node.x, node.y))
+    close_list[(node.x, node.y)] = node
 
-    def getDown(self):
-        (x, y) = self.pos
-        return getPosCell((x, y + 1))
+    _adjacent = []
 
-    def getLeft(self):
-        (x, y) = self.pos
-        return getPosCell((x - 1, y))
+    # if checkValid(node.x - 1, node.y - 1):
+    # _adjacent.append(Node(node, node.x - 1, node.y - 1))
 
-    def getRight(self):
-        (x, y) = self.pos
-        return getPosCell((x + 1, y))
+    if checkValid(node.x, node.y - 1):
+        _adjacent.append(Node(node, node.x, node.y - 1))
 
-    def calcPossibleDir(self):
-        pass
+    # if checkValid(node.x + 1, node.y - 1):
+    # _adjacent.append(Node(node, node.x + 1, node.y - 1))
 
-    def selectDir(self, dstPos):
-        pass
+    if checkValid(node.x + 1, node.y):
+        _adjacent.append(Node(node, node.x + 1, node.y))
 
-    def calcF(self):
-        self.F = self.G + self.H
+    # if checkValid(node.x + 1, node.y + 1):
+    # _adjacent.append(Node(node, node.x + 1, node.y + 1))
 
-    def calcG(self):  #起点到当前移动量
-        pass
+    if checkValid(node.x, node.y + 1):
+        _adjacent.append(Node(node, node.x, node.y + 1))
 
-    def calcH(self):  #当前到终点预估移动量
-        pass
+    # if checkValid(node.x - 1, node.y + 1):
+    # _adjacent.append(Node(node, node.x - 1, node.y + 1))
+
+    if checkValid(node.x - 1, node.y):
+        _adjacent.append(Node(node, node.x - 1, node.y))
+
+    for a in _adjacent:
+        if (a.x, a.y) == (end.x, end.y):
+            new_G = calc_G(a, node) + node.G
+            end.reset_father(node, new_G)
+            return True
+
+        if (a.x, a.y) in close_list:
+            continue
+
+        if (a.x, a.y) not in open_list:
+            open_list[(a.x, a.y)] = a
+        else:
+            exist_node = open_list[(a.x, a.y)]
+            new_G = calc_G(a, node) + node.G
+            if new_G < exist_node.G:
+                exist_node.reset_father(node, new_G)
+
+    return False
+
+
+def find_the_path(start, end):
+    global tracklist, block_list
+
+    open_list.clear()
+    close_list.clear()
+    del tracklist[:]
+
+    for p in block_list:
+        (posx, posy) = p
+        block_node = Node(None, posx, posy)
+        close_list[(block_node.x, block_node.y)] = block_node
+
+    open_list[(start.x, start.y)] = start
+    the_node = start
+    i = 0
+
+    while not addAdjacentIntoOpen(the_node):
+        i = i + 1
+        the_node = min_F_node()
+        if i > xcnt * ycnt:
+            print("quit")
+            return False
+
+    return True
+
+
+def mark_path(node):
+    if node.father == None:
+        return
+
+    pos = (node.x, node.y)
+    tracklist.append(pos)
+    mark_path(node.father)
 
 
 def getPosxyByXy((x, y)):
@@ -135,15 +210,13 @@ def fillRect(pos, color=WHITE):
 
 
 def genRdBlockList(cnt=BLOCKCNT):
-    blocklist = []
     for i in xrange(cnt):
         posx = random.randint(0, xcnt)
         posy = random.randint(0, ycnt)
         if (posx, posy) == (0, 0):
             pass
-        elif (posx, posy) not in blocklist:
-            blocklist.append((posx, posy))
-    return blocklist
+        elif (posx, posy) not in block_list:
+            block_list.append((posx, posy))
 
 
 def drawGrid(color=WHITE):
@@ -154,41 +227,47 @@ def drawGrid(color=WHITE):
 
 
 def genMap():
-    for x in xrange(0, xcnt + 1):
-        for y in xrange(0, ycnt + 1):
-            pos = (x, y)
-            c = Cell(pos)
-            maplist[pos] = c
-
-    blist = genRdBlockList()
-    for p in blist:
-        c = maplist[p]
-        c.setBlock()
+    genRdBlockList()
+    for p in block_list:
+        (posx, posy) = p
+        block_node = Node(None, posx, posy)
+        close_list[(block_node.x, block_node.y)] = block_node
 
 
 def drawMap():
     drawGrid((30, 30, 30))
-    for v in maplist.values():
-        if v.isBlock():
-            fillRect(v.getPos())
-        elif v.isEmpty():
-            pass
-        elif v.isStart():
-            fillRect(v.getPos(), (0, 0, 255))
-        elif v.isEnd():
-            fillRect(v.getPos(), (255, 0, 0))
-        elif v.isTrack():
-            fillRect(v.getPos(), (0, 255, 0))
+    for k in block_list:
+        fillRect(k)
+
+    for t in tracklist:
+        fillRect(t, (0, 255, 0))
+
+    if start:
+        fillRect((start.x, start.y), (0, 0, 255))
+    if end:
+        fillRect((end.x, end.y), (255, 0, 0))
 
 
-def moveTo(posFrom, posDst):
-    c = maplist[posFrom]
-    if posFrom == posDst:
-        print("move to dst postion success")
-    else:
-        pass
-    c.setTrack()
-    return c
+def findThePaths(cnt=3):
+    global tracklistRec
+    global tracklist
+
+    tracklistRec.clear()
+
+    for x in xrange(cnt):
+        del tracklist[:]
+        if find_the_path(start, end):
+
+            mark_path(end.father)
+            if len(tracklist) > 0:
+                l = copy.deepcopy(tracklist)
+                tracklistRec[len(l)] = l
+                print("num=", x, "key=", len(l))
+
+    if len(tracklistRec) > 0:
+        k = sorted(tracklistRec.keys())
+        print("choose=", k[0])
+        tracklist = tracklistRec[k[0]]
 
 
 if __name__ == "__main__":
@@ -198,6 +277,8 @@ if __name__ == "__main__":
     FPSCLOCK = pygame.time.Clock()
 
     genMap()
+
+    flag = False
 
     while True:
         SCREEN.fill((0, 0, 0))
@@ -210,24 +291,24 @@ if __name__ == "__main__":
                 for index in xrange(len(pressed_array)):
                     if pressed_array[index]:
                         if index == 0:
-                            # print("Pressed LEFT Button!")
-                            p = getPosxyByXy(pygame.mouse.get_pos())
-                            c = maplist[p]
-                            c.setStart()
+                            (x, y) = getPosxyByXy(pygame.mouse.get_pos())
+                            start = Node(None, x, y)
                         elif index == 1:
                             print("The mouse whell Pressed!")
                         elif index == 2:
-                            # print("Pressed RIGHT Button!")
-                            p = getPosxyByXy(pygame.mouse.get_pos())
-                            c = maplist[p]
-                            c.setEnd()
-            # elif event.type == MOUSEMOTION:
-            # pos = pygame.mouse.get_pos()
-            # mouse_x = pos[0]
-            # mouse_y = pos[1]
+                            (x, y) = getPosxyByXy(pygame.mouse.get_pos())
+                            end = Node(None, x, y)
+                            flag = True
+
+        if flag:
+            findThePaths()
+            flag = False
+
+        # if flag and find_the_path(start, end):
+        # mark_path(end.father)
+        # flag = False
 
         drawMap()
-        moveTo((0, 0), (5, 5))
 
         FPSCLOCK.tick(FPS)
         pygame.display.update()
---
+++ my_dfs.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+#       FileName:  my_astar.py
+#
+#    Description:
+#
+#        Version:  1.0
+#        Created:  2018-06-19 16:22:23
+#  Last Modified:  2018-06-23 10:36:16
+#       Revision:  none
+#       Compiler:  gcc
+#
+#         Author:  zt ()
+#   Organization:
+
+import os
+import math
+import pygame
+from pygame.locals import *
+from sys import exit
+import random
+from PIL import Image
+
+delta = 20
+
+display_width, display_height = 800, 600
+display_res = (display_width + 1, display_height + 1)
+
+BLOCKCNT = int((display_width / delta) * (display_height / delta) / 8)
+
+xcnt = int((display_width / delta) - 1)
+ycnt = int((display_height / delta) - 1)
+
+global SCREEN, FPSCLOCK
+FPS = 5
+
+WHITE = (255, 255, 255)
+
+startPoint = (0, 0)
+targetPoint = (xcnt, ycnt)
+# targetPoint = (2, 2)
+currentPoint = None
+
+stepComplete = False
+allComplete = False
+
+moveDirection = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+arrive_list = []
+block_list = []
+back_list = []
+
+maxstepCnts = xcnt * ycnt
+
+
+def IsAtDestination(cur, des):
+    if cur == des:
+        return True
+    return False
+
+
+def NextStep(pos, arrive_list, block_list, back_list):
+    (x, y) = pos
+    global currentPoint
+
+    if not (0 <= x <= xcnt):
+        return False
+
+    if not (0 <= y <= ycnt):
+        return False
+
+    if pos in arrive_list:
+        return False
+
+    if pos in block_list:
+        return False
+
+    if pos in back_list:
+        return False
+
+    currentPoint = pos
+    arrive_list.append(pos)
+    return True
+
+
+def BackStep(arrive_list, back_list):
+    global currentPoint
+    back_list.append(currentPoint)
+    if len(arrive_list) > 1:
+        arrive_list.pop()
+        currentPoint = arrive_list[-1]
+        return True
+    return False
+
+
+def DfsAlg(block_list, arrive_list, back_list):
+    global stepComplete, allComplete, currentPoint
+    currentPoint = startPoint
+
+    i = 0
+
+    while not allComplete:
+        stepComplete = False
+
+        while not stepComplete:
+            (cx, cy) = currentPoint
+            if IsAtDestination(currentPoint, targetPoint):
+                allComplete = True
+                break
+
+            mvlist = [(cx + x, cy + y) for (x, y) in moveDirection]
+            for pos in mvlist:
+                if NextStep(pos, arrive_list, block_list, back_list):
+                    stepComplete = True
+                    break
+            if not stepComplete:
+                BackStep(arrive_list, back_list)
+        i += 1
+        if i > maxstepCnts:
+            print(".............exit")
+            break
+
+
+def DfsAlg2(block_list, arrive_list, back_list):
+    global stepComplete, allComplete, currentPoint
+
+    if not currentPoint:
+        currentPoint = startPoint
+
+    if allComplete:
+        print("allComplete")
+        return
+
+    if IsAtDestination(currentPoint, targetPoint):
+        allComplete = True
+        stepComplete = True
+        print("IsAtDestination")
+        return
+
+    stepComplete = False
+
+    while not stepComplete:
+        (cx, cy) = currentPoint
+        mvlist = [(cx + x, cy + y) for (x, y) in moveDirection]
+
+        for pos in mvlist:
+            if NextStep(pos, arrive_list, block_list, back_list):
+                stepComplete = True
+                break
+
+        if not stepComplete:
+            if not BackStep(arrive_list, back_list):
+                allComplete = True
+                print("error.........")
+                break
+
+
+def getPosxyByXy((x, y)):
+    return (int(math.ceil(x / delta)), int(math.ceil(y / delta)))
+
+
+def fillRect(pos, color=WHITE):
+    (x, y) = pos
+    pygame.draw.rect(SCREEN, color, (x * delta, y * delta, delta, delta), 0)
+
+
+def genMap(cnt=BLOCKCNT):
+    global block_list
+    for i in xrange(cnt):
+        x = random.randint(0, xcnt)
+        y = random.randint(0, ycnt)
+        block_list.append((x, y))
+
+
+def drawGrid(color=WHITE):
+    for x in xrange(0, display_width + 1, delta):
+        pygame.draw.line(SCREEN, color, (x, 0), (x, display_height), 1)
+    for y in xrange(0, display_height + 1, delta):
+        pygame.draw.line(SCREEN, color, (0, y), (display_width, y), 1)
+
+
+def drawMap():
+    drawGrid((30, 30, 30))
+    for p in block_list:
+        fillRect(p, (255, 255, 255))
+
+    for p in arrive_list:
+        fillRect(p, (0, 255, 0))
+
+    for p in back_list:
+        fillRect(p, (0, 128, 0))
+
+    fillRect(startPoint, (0, 0, 255))
+    fillRect(targetPoint, (255, 0, 0))
+
+
+if __name__ == "__main__":
+    pygame.init()
+    SCREEN = pygame.display.set_mode(display_res, 0, 32)
+    pygame.display.set_caption('slam')
+    FPSCLOCK = pygame.time.Clock()
+
+    genMap()
+
+    # flag = False
+    flag = True
+
+    while True:
+        SCREEN.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+            elif event.type == MOUSEBUTTONDOWN:
+                pressed_array = pygame.mouse.get_pressed()
+                for index in xrange(len(pressed_array)):
+                    if pressed_array[index]:
+                        if index == 0:
+                            # print("Pressed LEFT Button!")
+                            startPoint = getPosxyByXy(pygame.mouse.get_pos())
+                        elif index == 1:
+                            print("The mouse whell Pressed!")
+                        elif index == 2:
+                            # print("Pressed RIGHT Button!")
+                            targetPoint = getPosxyByXy(pygame.mouse.get_pos())
+                            flag = True
+
+        if flag:
+            # arrive_list = []
+            # back_list = []
+            # stepComplete = False
+            # allComplete = False
+            # DfsAlg(block_list, arrive_list, back_list)
+            # flag = False
+            DfsAlg2(block_list, arrive_list, back_list)
+        drawMap()
+
+        FPSCLOCK.tick(FPS)
+        pygame.display.update()
